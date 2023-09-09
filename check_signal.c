b32b66487293bda84ea15a31b07fa8bdd7c5ba24
rabeloguedes <rabeloguedes@proton.me>
2023-09-09 18:44:29 +0100

    feat: Adds a function which acknowledgesif the message was sucessfully receveid.

---
+++ check_signal.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_signal.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/06 18:01:08 by arabelo-          #+#    #+#             */
+/*   Updated: 2023/09/06 18:02:59 by arabelo-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./inc/minitalk.h"
+
+void	check_signal(int signal)
+{
+	if (signal == SIGUSR1)
+		ft_printf("Message sucessfully received!\n");
+	else
+		ft_printf("Something went wrong, message unsucessfully received!\n");
+}
