cbf65fa3e6e1fe9e71c8ca3328fab4484ccfd8c4
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-09-12 11:51:55 +0100

    refactor: Removes the unnecessary pointer to u_char from the parameters of the handle_string and simplifies the handle_signal function in the mandatory version.

--- handle_signal.c
+++ handle_signal.c
@@ -6,20 +6,19 @@
 /*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 23:38:59 by arabelo-          #+#    #+#             */
-/*   Updated: 2023/09/09 18:34:49 by arabelo-         ###   ########.fr       */
+/*   Updated: 2023/09/12 07:57:21 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./inc/minitalk.h"
 
-void	handle_signal(int signal, struct __siginfo *siginfo, void *addr)
+void	handle_signal(int signal)
 {
 	static int				bits_counter;
 	static unsigned char	c;
 	static char				*message;
 	static int				chars_counter;
 
-	(void)addr;
 	if (signal == SIGUSR1)
 		c |= (1 << bits_counter);
 	if (!chars_counter && !c && !bits_counter)
@@ -28,7 +27,7 @@ void	handle_signal(int signal, struct __siginfo *siginfo, void *addr)
 	if (bits_counter > 7)
 	{
 		bits_counter = 0;
-		if (!handle_string(message, &c, &chars_counter))
+		if (!handle_string(message, c, &chars_counter))
 			return ;
 		message = get_string(message, c, chars_counter);
 		c = 0;
--- handle_string.c
+++ handle_string.c
@@ -6,15 +6,15 @@
 /*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 20:22:38 by arabelo-          #+#    #+#             */
-/*   Updated: 2023/09/08 17:35:20 by arabelo-         ###   ########.fr       */
+/*   Updated: 2023/09/12 07:57:04 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./inc/minitalk.h"
 
-int	handle_string(char *message, unsigned char *c, int *chars_counter)
+int	handle_string(char *message, unsigned char c, int *chars_counter)
 {
-	if (!*c)
+	if (!c)
 	{
 		*chars_counter = 0;
 		if (message)
