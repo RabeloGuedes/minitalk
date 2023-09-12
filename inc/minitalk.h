9e76aed112c6ce09566bcb95196fad1147149f67
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2023-09-12 11:52:30 +0100

    refactor: Change the prototypes of some functions.

--- inc/minitalk.h
+++ inc/minitalk.h
@@ -6,7 +6,7 @@
 /*   By: arabelo- <arabelo-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/20 22:24:19 by arabelo-          #+#    #+#             */
-/*   Updated: 2023/09/09 18:38:04 by arabelo-         ###   ########.fr       */
+/*   Updated: 2023/09/12 09:25:04 by arabelo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,14 +19,15 @@
 
 # include "../libft/inc/libft.h"
 # include <signal.h>
+# include <sys/types.h>
 
 void	send_signal(pid_t server_pid, unsigned char c);
 void	send_string(pid_t server_pid, char *str);
-void	handle_signal(int signal, struct __siginfo *siginfo, void *addr);
-void	handle_signal_bonus(int signal, struct __siginfo *siginfo, void *addr);
+void	handle_signal(int signal);
+void	handle_signal_bonus(int signal, siginfo_t *siginfo, void *addr);
 void	check_signal(int signal);
-void	sigaction_config(struct	sigaction *sa);
+void	sigaction_config(struct sigaction *sa);
 char	*get_string(char *old_message, unsigned char c, int chars_counter);
-int		handle_string(char *message, unsigned char *c, int *chars_counter);
-
+int		handle_string(char *message, unsigned char c, int *chars_counter);
+int		is_pid_valid(char *pid_str);
 #endif
